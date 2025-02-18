924829ac7c2e07aecbefd9a6ef68bdeb53bfffc5
Andrey <systerr@gmail.com>
2025-02-18 14:12:32 +0000

    update post and prebuild

--- postBuild.ts
+++ postBuild.ts
@@ -1,1 +1,14 @@
-console.log("hi there");
+import fs from "node:fs/promises";
+
+console.info("postBuild. Coping files");
+console.time("postBuild. Done");
+
+const paths = ["resources", "templates", "types.d.ts"];
+
+const promises = paths.map((path) => {
+  fs.cp(`./src/${path}`, `./dist/${path}`, { recursive: true });
+});
+const result = await Promise.all(promises);
+
+console.log(result);
+console.timeEnd("postBuild. Done");
